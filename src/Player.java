0fde27c10f23802753b0043b917306de818e44c8
Sławek <slaw.stasz@gmial.com>
2019-11-22 11:39:52 +0100

    drobne poprawki

--- src/Player.java
+++ src/Player.java
@@ -11,17 +11,30 @@ public class Player {
         this.currentSpeed = currentSpeed;
     }
 
+    public double checkPossibleAcceleration() {
+        double temporaryAcceleration;
+        if (currentSpeed < 0.8 * CarList.MALUCH.getTopSpeed()) {
+            temporaryAcceleration = CarList.MALUCH.getAcceleration() * 0.3;
+        } else if (currentSpeed < 0.5 * CarList.MALUCH.getTopSpeed()) {
+            temporaryAcceleration = CarList.MALUCH.getAcceleration() * 0.7;
+        } else {
+            temporaryAcceleration = CarList.MALUCH.getAcceleration();
+        }
+        return temporaryAcceleration;
+    }
+
     public double drive() {
         currentSpeed =
-                (currentSpeed + CarList.MALUCH.getAcceleration()) * CurvesList.DEGREES90.getSpeedLossParameter() * DriverList.JESUSTAKESTHEWHEEL.getSkillLevel();
+                (currentSpeed + checkPossibleAcceleration()) * CurvesList.DEGREES90.getSpeedLossParameter() * DriverList.JESUSTAKESTHEWHEEL.getSkillLevel();
 
         return currentSpeed;
     }
 
-    public void checkSpeed(){
-        if(currentSpeed>=CarList.MALUCH.getTopSpeed()){
-            currentSpeed=CarList.MALUCH.getTopSpeed();
+    public void checkSpeed() {
+        if (currentSpeed >= CarList.MALUCH.getTopSpeed()) {
+            currentSpeed = CarList.MALUCH.getTopSpeed();
         }
         // jaki else?   {currentSpeed = currentSpeed ??
     }
+
 }
